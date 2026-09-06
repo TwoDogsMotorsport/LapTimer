9ef97bc3cb5b2bf071c97869b65d5338362af707
Christopher Moran <chris.moran@emergent.com.au>
2026-09-06 22:35:27 +1000

    Fixed LCD Formatting (I hope)

--- Receiver/Receiver.ino
+++ Receiver/Receiver.ino
@@ -9,6 +9,8 @@
 LiquidCrystal_I2C lcd(LCD_ADDR, LCD_COLS, LCD_ROWS);
 
 void setup() {
+  int i = 0;
+  
   Serial.begin(115200);
   while (!Serial);                   // wait for USB serial on some boards
 
@@ -22,6 +24,11 @@ void setup() {
 
   /* Start the IR module */
   IrReceiver.begin(IR_RECEIVE_PIN, ENABLE_LED_FEEDBACK);  // LED_BUILTIN blinks on receive
+
+  /* Reset the time history */
+  history_counter = 0;
+  for(i=0; i < HISTORY_SIZE; i++)
+    history[i] = 0;
 
   Serial.println(F("IR Beacon Receiver ready"));
   Serial.print(F("Looking for ID 0x"));
@@ -43,5 +50,44 @@ void loop() {
     }
 
     IrReceiver.resume();             // ready for the next frame
+  }
+}
+
+void startClock(void) {
+  startmillis = millis();
+}
+
+void stopClock(void) {
+  millisecs = millis() - startmillis;
+  /* Save the time */
+  if (history_counter < (HISTORY_SIZE -1)) {
+    history[history_counter] = millisecs;
+    history_counter++;
+  }
+}
+
+/* We have time to do all this
+ * The display will look like:
+ * nn hh:mm:ss.ms
+ * where  nn  = lap #
+ *        hhh = Hours
+ *        mm  = Minutes
+ *        ss  = Seconds
+ *        ms  = Milliseconds
+*/
+
+void calcTime(unsigned long worktime, unsigned int lap, unsigned int row) {
+  char buffer[LCD_COLS];
+  unsigned long ms = worktime % 1000;
+  unsigned long w_secs = worktime / 1000;
+  unsigned long secs = w_secs % 60;
+  unsigned long w_min = w_secs / 60;
+  unsigned long mins = w_min % 60;
+  unsigned long hrs = w_min / 60;
+
+  if(row >=0 && row < LCD_ROWS) {
+    lcd.setCursor(0, row);
+    snprintf(buffer, sizeof(buffer), "%03d:%02d:%02d.%03d", hrs, mins, secs, ms);
+    lcd.print(buffer);
   }
 }
--- Receiver/receiver.h
+++ Receiver/receiver.h
@@ -1,11 +1,29 @@
 #include <LiquidCrystal_I2C.h>
 #include <IRremote.hpp>
 
+#define _DEBUG              1
+#define HISTORY_SIZE        32
+
 // ========== CONFIGURE THIS ==========
-#define EXPECTED_ID        0xA5      // must match the transmitter's BEACON_ID
-#define IR_RECEIVE_PIN     2         // TSOP OUT pin (interrupt-capable recommended)
+#define EXPECTED_ID         0xA5      // must match the transmitter's BEACON_ID
+#define IR_RECEIVE_PIN      2         // TSOP OUT pin (interrupt-capable recommended)
+
+#define LCD_ROWS            4         // Available rows on LCD
+#define LCD_COLS            20        // Available columns
+#define LCD_ADDR            0x3F      // Default I2C address
 // ====================================
 
-#define LCD_ROWS      4
-#define LCD_COLS      20
-#define LCD_ADDR      0x3F
+unsigned long millisecs;
+unsigned long startmillis;
+
+/* The next part is experimental for now.
+ *  Track a number of laps using a pre-defined
+ *  block of memory
+ */
+
+unsigned long history[HISTORY_SIZE];
+unsigned short history_counter;
+
+void startClock(void);
+void stopClock(void);
+void calcTime(unsigned long worktime, unsigned int lap, unsigned int row);
