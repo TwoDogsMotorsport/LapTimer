5f121cf9637c18deae39b7b36c140c03ce54c712
Two Dogs Motorsport <twodogsmotorsport@gmail.com>
2026-09-10 13:26:43 +1000

    Tidy up for transmitter code

--- Transmitter/Transmitter.ino
+++ Transmitter/Transmitter.ino
@@ -1,16 +1,9 @@
 /*
  * Simple IR Beacon Transmitter
  * Continuously sends an 8-bit ID using NEC protocol.
- * Change BEACON_ID below to make multiple beacons unique.
  */
 
-#include <IRremote.hpp>
-
-// ========== CONFIGURE THIS ==========
-#define BEACON_ID          0xA5      // 8-bit ID (0x00 – 0xFF)
-#define IR_SEND_PIN        3         // PWM pin for IR LED
-#define SEND_INTERVAL_MS   100       // how often to transmit (ms)
-// ====================================
+#include "transmitter.h"
 
 void setup() {
   Serial.begin(115200);
